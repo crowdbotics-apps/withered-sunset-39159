aac686ffc6f768eef22d9aaf729b3b7705813347
Crowdbotics <team@crowdbotics.com>
2023-02-28 20:01:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "withered-sunset-39159",
+  url: "https://withered-sunset-39159.botics.co",
+  api: "https://withered-sunset-39159.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
